4a50972ccb433e56c9e7d741154f406a961503af
jaeseongDev <qkrwotjd1445@naver.com>
2021-07-05 17:06:27 +0900

    chore: 초기 환경 설정

---
+++ backend/src/main/java/com/darass/darass/DarassApplication.java
@@ -0,0 +1,13 @@
+package com.darass.darass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarassApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/darass/darass/DarassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darass.darass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
