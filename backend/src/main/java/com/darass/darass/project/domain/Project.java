d0927fede807b35c12f6f8c4cc5339d822a58a2f
jujubebat <bbwwpark@naver.com>
2021-07-07 15:47:08 +0900

    feat: Project 엔티티 구현

---
+++ backend/src/main/java/com/darass/darass/project/domain/Project.java
@@ -0,0 +1,27 @@
+package com.darass.darass.project.domain;
+
+import com.darass.darass.user.domain.User;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    private String name;
+
+    private String secretKey;
+
+}
