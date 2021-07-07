c904f82009ab69953f4e363529f90fd8fe71a23e
jujubebat <bbwwpark@naver.com>
2021-07-07 15:33:41 +0900

    refactor: User 클래스를 추상클래스로 변경, 생성자 추가

--- backend/src/main/java/com/darass/darass/user/domain/GuestUser.java
+++ backend/src/main/java/com/darass/darass/user/domain/GuestUser.java
@@ -1,9 +1,17 @@
 package com.darass.darass.user.domain;
 
 import javax.persistence.Entity;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Entity
 public class GuestUser extends User {
 
     private String password;
+
+    private GuestUser(String nickName, String password) {
+        super(nickName);
+        this.password = password;
+    }
+
 }
