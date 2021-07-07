ba367e09f5a791336f6c2b06035511f0ebff2b44
JinYoung Park <basejin0810@naver.com>
2021-07-07 16:08:31 +0900

    feat: 댓글 테이블 매핑

--- backend/src/main/java/com/darass/darass/comment/domain/Comment.java
+++ backend/src/main/java/com/darass/darass/comment/domain/Comment.java
@@ -1,15 +1,29 @@
 package com.darass.darass.comment.domain;
 
-import javax.persistence.*;
+import com.darass.darass.project.domain.Project;
+import com.darass.darass.user.domain.User;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-//@Entity
+@Entity
 public class Comment {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @OneToOne
-//    private User user;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    @ManyToOne
+    @JoinColumn
+    private Project project;
+
+    private String url;
+    private String content;
 }
