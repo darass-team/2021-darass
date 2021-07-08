46b250829c26facfffff15877059f2c527549198
jaeseongDev <qkrwotjd1445@naver.com>
2021-07-08 18:52:46 +0900

    test: Acceptance 테스트 예시 코드 추가

---
+++ backend/src/test/java/com/darass/darass/AcceptanceTest.java
@@ -0,0 +1,65 @@
+package com.darass.darass;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+import com.darass.darass.comment.controller.dto.CommentResponse;
+import com.darass.darass.comment.repository.CommentRepository;
+import com.darass.darass.project.repository.ProjectRepository;
+import com.darass.darass.user.domain.GuestUser;
+import com.darass.darass.user.domain.User;
+import com.darass.darass.user.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@SpringBootTest
+public class AcceptanceTest {
+
+    protected MockMvc mockMvc;
+
+    protected User user;
+
+    @Autowired
+    private UserRepository users;
+    @Autowired
+    private ProjectRepository projects;
+    @Autowired
+    private CommentRepository comments;
+
+    // Json을 Object로 변환 (Jackson을 활용한 것)
+    protected static String asJsonString(Object obj) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(obj);
+    }
+
+    // Object를 Json으로 변환
+    protected <T> T asObject(String json, Class<T> objectClass) throws JsonProcessingException {
+        return new ObjectMapper().readValue(json, objectClass);
+    }
+
+    @BeforeEach
+    public void setUp(WebApplicationContext webApplicationContext,
+        RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(documentationConfiguration(restDocumentation))
+            .build();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        comments.deleteAll();
+        projects.deleteAll();
+        users.deleteAll();
+    }
+}
