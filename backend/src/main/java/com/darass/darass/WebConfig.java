9e6d2c332067c0736ecc48c026a214eba6e3d202
JinYoung Park <basejin0810@naver.com>
2021-07-08 17:05:45 +0900

    fix: PATCH, DELETE 메소드에 대해 CORS 이슈가 발생하는 버그 해결 (#27)

--- backend/src/main/java/com/darass/darass/WebConfig.java
+++ backend/src/main/java/com/darass/darass/WebConfig.java
@@ -9,6 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*");
     }
 }
