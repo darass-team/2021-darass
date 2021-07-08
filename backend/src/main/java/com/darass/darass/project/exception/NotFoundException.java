1c1c378167b615f3ba08d2bc31429ce4802f9016
Sehwan_Jang <68985748+Sehwan-Jang@users.noreply.github.com>
2021-07-08 17:48:48 +0900

    [BE] 프로젝트 api 구현 (#31)
    * feat: 프로젝트 추가 기능 구현
    
    * feat: 사용자 별 프로젝트 조회 기능 구현
    
    * feat: 프로젝트 삭제  기능 구현
    
    * feat: 단일 프로젝트 조회  기능 구현
    
    * feat: 아이디 notfound 예외처리 추가

---
+++ backend/src/main/java/com/darass/darass/project/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.darass.darass.project.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
