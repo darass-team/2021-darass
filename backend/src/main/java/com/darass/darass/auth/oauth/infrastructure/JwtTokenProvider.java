9b6b442fdd36a41eaeb20d6fe323ff5e50520690
Sehwan_Jang <68985748+Sehwan-Jang@users.noreply.github.com>
2021-07-08 15:22:04 +0900

    [BE] Oauth 관련 API 구현 (#12)
    * feat: 유저 테이블 매핑
    
    * refactor: User 클래스를 추상클래스로 변경, 생성자 추가
    
    * feat: Project 엔티티 구현
    
    * feat: 댓글 테이블 매핑
    
    * feat: 각 도메인에 해당하는 Repository 인터페이스 생성
    
    * feat: 카카오 사용자 정보를 받아오는 기능 구현
    
    * feat: jwt 토큰 생성 기능 구현
    
    * refactor: oauth 로그인 로직 리팩토링
    
    * refactor: 토큰 시크릿키와 만료기한 값을 설정 파일로 분리
    
    * feat: 카카오 api 서버 관련 테스트 작성 및 인증 관련 코드 리팩토링
    
    * feat: 새로운 사용자인 경우에만 사용자 정보를 DB에 저장하도록 구현
    
    Co-authored-by: jujubebat <bbwwpark@naver.com>
    Co-authored-by: JinYoung Park <basejin0810@naver.com>
    Co-authored-by: jaeseongDev <qkrwotjd1445@naver.com>

---
+++ backend/src/main/java/com/darass/darass/auth/oauth/infrastructure/JwtTokenProvider.java
@@ -0,0 +1,44 @@
+package com.darass.darass.auth.oauth.infrastructure;
+
+import com.darass.darass.auth.oauth.exception.AuthenticationException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+    @Value("${security.jwt.token.expire-length}")
+    private long validityInMilliseconds;
+
+    public String createToken(String payload) {
+        Claims claims = Jwts.claims().setSubject(payload);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(now)
+            .setExpiration(validity)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+    }
+
+    public String getPayload(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new AuthenticationException("유효하지 않은 토큰 입니다.");
+        }
+    }
+}
