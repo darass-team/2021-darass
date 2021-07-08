a67539ac675b4f06425bf9d39f3723689087837a
Byung Wook Park <bbwwpark@naver.com>
2021-07-08 18:25:13 +0900

    feat: 댓글 api에 어떠한 유저가 댓글을 달았는지 판단하는 기능 구현 (#26)
    Co-authored-by: JinYoung Park <basejin0810@naver.com>

--- backend/src/main/java/com/darass/darass/auth/oauth/AuthControllerAdvice.java
+++ backend/src/main/java/com/darass/darass/auth/oauth/AuthControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.darass.darass.auth.oauth;
 
 import com.darass.darass.auth.oauth.exception.AuthenticationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AuthControllerAdvice {
 
     @ExceptionHandler(AuthenticationException.class)
-    public String authenticationExceptionHandler(AuthenticationException exception) {
-        return exception.getMessage();
+    public ResponseEntity<String> authenticationExceptionHandler(AuthenticationException exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
     }
 }
--- backend/src/main/java/com/darass/darass/auth/oauth/infrastructure/LoginInterceptor.java
+++ backend/src/main/java/com/darass/darass/auth/oauth/infrastructure/LoginInterceptor.java
@@ -12,13 +12,16 @@ public class LoginInterceptor implements HandlerInterceptor {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-//        if ("OPTIONS".equals(request.getMethod())) {
-//            return true;
-//        }
-//        final String accessToken = AuthorizationExtractor.extract(request);
-//        jwtTokenProvider.validateToken(accessToken);
-//        return true;
-//    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if ("OPTIONS".equals(request.getMethod())) {
+            return true;
+        }
+        final String accessToken = AuthorizationExtractor.extract(request);
+        if(accessToken == null){
+            return true;
+        }
+        jwtTokenProvider.validateToken(accessToken);
+        return true;
+    }
 }
