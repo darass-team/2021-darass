91d887dcf3d5cbd09d97e2de83252623734468a3
Aaron <whilter08@gmail.com>
2021-07-09 18:19:51 +0900

    chore: maria db 설정 추가

--- backend/src/test/java/com/darass/darass/AcceptanceTest.java
+++ backend/src/test/java/com/darass/darass/AcceptanceTest.java
@@ -16,8 +16,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -25,6 +27,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest
+@ActiveProfiles("test")
 public class AcceptanceTest {
 
     protected MockMvc mockMvc;
