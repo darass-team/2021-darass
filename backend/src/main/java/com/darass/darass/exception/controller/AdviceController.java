b997af2f2d11ec262249e36c150fc844da0c5146
jaeseongDev <qkrwotjd1445@naver.com>
2021-07-10 14:30:27 +0900

    feat: 예외 처리 클래스 정리

---
+++ backend/src/main/java/com/darass/darass/exception/controller/AdviceController.java
@@ -0,0 +1,46 @@
+package com.darass.darass.eception.controller;
+
+import com.darass.darass.exception.httpbasicexception.BadRequestException;
+import com.darass.darass.exception.httpbasicexception.ConflictException;
+import com.darass.darass.exception.ExceptionWithMessageAndCode;
+import com.darass.darass.exception.httpbasicexception.NotFoundException;
+import com.darass.darass.exception.httpbasicexception.UnauthorizedException;
+import com.darass.darass.exception.dto.ExceptionResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AdviceController {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionResponse handle(BadRequestException e) {
+        return new ExceptionResponse(e.getMessage(), e.getCode());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ExceptionResponse handle(ConflictException e) {
+        return new ExceptionResponse(e.getMessage(), e.getCode());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionResponse handle(NotFoundException e) {
+        return new ExceptionResponse(e.getMessage(), e.getCode());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ExceptionResponse handle(UnauthorizedException e) {
+        return new ExceptionResponse(e.getMessage(), e.getCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionResponse handle(Exception e) {
+        return new ExceptionResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+}
