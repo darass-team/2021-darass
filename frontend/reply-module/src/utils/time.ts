d23a2398bd803d114217d1747f83ef29428b3d4c
zereight <fullstackmachinelearning@gmail.com>
2021-07-12 15:06:26 +0900

    feat: week 단위 추가

--- frontend/reply-module/src/utils/time.ts
+++ frontend/reply-module/src/utils/time.ts
@@ -3,6 +3,7 @@ const TIME_TABLE_BY_MILLISECOND = {
   MINUTE: 60 * 1000,
   HOUR: 60 * 60 * 1000,
   DAY: 24 * 60 * 60 * 1000,
+  WEEK: 7 * 24 * 60 * 60 * 1000,
   MONTH: 30 * 24 * 60 * 60 * 1000,
   YEAR: 12 * 30 * 24 * 60 * 60 * 1000
 };
@@ -16,7 +17,8 @@ const getTimeDifference = (targetTime: string) => {
   if (diff < TIME_TABLE_BY_MILLISECOND.MINUTE) return `방금 전`;
   else if (diff < TIME_TABLE_BY_MILLISECOND.HOUR) return `${Math.floor(diff / TIME_TABLE_BY_MILLISECOND.MINUTE)}분 전`;
   else if (diff < TIME_TABLE_BY_MILLISECOND.DAY) return `${Math.floor(diff / TIME_TABLE_BY_MILLISECOND.HOUR)}시간 전`;
-  else if (diff < TIME_TABLE_BY_MILLISECOND.MONTH) return `${Math.floor(diff / TIME_TABLE_BY_MILLISECOND.DAY)}일 전`;
+  else if (diff < TIME_TABLE_BY_MILLISECOND.WEEK) return `${Math.floor(diff / TIME_TABLE_BY_MILLISECOND.DAY)}일 전`;
+  else if (diff < TIME_TABLE_BY_MILLISECOND.MONTH) return `${Math.floor(diff / TIME_TABLE_BY_MILLISECOND.WEEK)}주 전`;
   else if (diff < TIME_TABLE_BY_MILLISECOND.YEAR) return `${Math.floor(diff / TIME_TABLE_BY_MILLISECOND.MONTH)}개월 전`;
   else return `${Math.floor(Math.floor(diff / TIME_TABLE_BY_MILLISECOND.YEAR))}년 전`;
 };
