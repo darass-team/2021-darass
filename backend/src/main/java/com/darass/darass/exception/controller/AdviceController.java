55e636c66f42e20381343b27ea1546cf28a3291f
Aaron <whilter08@gmail.com>
2021-07-16 20:02:29 +0900

    feat: 로깅 기능 추가

--- backend/src/main/java/com/darass/darass/exception/controller/AdviceController.java
+++ backend/src/main/java/com/darass/darass/exception/controller/AdviceController.java
@@ -4,6 +4,8 @@ import com.darass.darass.exception.dto.ExceptionResponse;
 import com.darass.darass.exception.httpbasicexception.ConflictException;
 import com.darass.darass.exception.httpbasicexception.NotFoundException;
 import com.darass.darass.exception.httpbasicexception.UnauthorizedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ControllerAdvice {
+public class AdviceController {
+    Logger log = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -41,6 +44,7 @@ public class ControllerAdvice {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse handleException(Exception e) {
         e.printStackTrace();
+        log.error("Unexpected Error From Server \n message : {} \n stacktrace : ", e.getMessage(), e);
         return new ExceptionResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
