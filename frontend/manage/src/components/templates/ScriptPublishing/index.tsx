c5b479bba66df40927c2f6637d92477a4c92c827
zereight <42544600+zereight@users.noreply.github.com>
2021-07-17 22:15:25 +0900

    [FE][관리자 페이지] 스크립트 코드 하이라이팅 추가 (#159) (#162)
    * feat: 스크립트 코드 하이라이팅 수정 및 콘텐츠 내용 수정
    
    * feat: 스크립트 복사 기능 추가

--- frontend/manage/src/components/templates/ScriptPublishing/index.tsx
+++ frontend/manage/src/components/templates/ScriptPublishing/index.tsx
@@ -1,5 +1,8 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { xcode } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { useCopyButton } from "../../../hooks";
 import ScreenContainer from "../../../styles/ScreenContainer";
-import { Container, Section, Title, CodeBlockWrapper, P } from "./styles";
+import { B, CodeBlockWrapper, Container, Content, CopyButton, P, Section, Title } from "./styles";
 
 const scriptCode = (projectSecretKey: string) => `
 <!-- 다라쓰 설치 코드 -->
@@ -17,7 +20,7 @@ const scriptCode = (projectSecretKey: string) => `
     </script>
     <noscript>다라쓰 댓글 작성을 위해 JavaScript를 활성화 해주세요</noscript>
 </div>
-<!-- 다라쓰 설치 코드 끝 --> 
+<!-- 다라쓰 설치 코드 끝 -->
 `;
 
 export interface Props {
@@ -25,26 +28,51 @@ export interface Props {
 }
 
 const ScriptPublishing = ({ projectSecretKey }: Props) => {
+  const script = scriptCode(projectSecretKey || "코드를 불러오는 중입니다...");
+  const { isCopyButtonClicked, onCopy } = useCopyButton();
+
   return (
     <ScreenContainer>
       <Container>
         <Section>
-          <Title>다라쓰 설치 코드</Title>
-          <CodeBlockWrapper>{scriptCode(projectSecretKey || "코드를 불러오는 중입니다...")}</CodeBlockWrapper>
+          <Title>스크립트</Title>
+          <CodeBlockWrapper>
+            <CopyButton type="button" onClick={() => onCopy(script)}>
+              {isCopyButtonClicked ? "Copied !" : "Copy"}
+            </CopyButton>
+            <SyntaxHighlighter language="javascript" style={xcode}>
+              {script}
+            </SyntaxHighlighter>
+          </CodeBlockWrapper>
         </Section>
 
         <Section>
-          <Title>코드 적용 방법</Title>
-          <P>
-            Condimentum venenatis blandit massa proin nunc, curabitur sit. Lacus dolor ut aliquet semper lorem eros.
-            Ultrices id scelerisque aliquam mattis egestas. Orci ornare varius amet ornare. Nulla nullam imperdiet
-            hendrerit cras enim quis nisi cursus. Elit massa adipiscing fermentum imperdiet vulputate iaculis malesuada.
-            Urna aliquet vitae enim, id in luctus. Risus aliquam amet eu, fames iaculis mauris, metus, sem nascetur.
-            Vulputate ac nibh proin nisi eu fermentum eget eu nulla. Cum id in convallis urna fringilla quam congue
-            turpis. Nibh volutpat a vestibulum tempor purus odio nunc est lacus. Eros libero sed tortor aliquam purus
-            ut. Faucibus at vulputate molestie congue convallis proin proin duis et. Velit tincidunt facilisis vel orci
-            adipiscing id et, vel.
-          </P>
+          <Title>스크립트 적용 가이드</Title>
+
+          <Content>
+            <B>1. 다라쓰 코드 설치</B>
+            <P> 다라쓰를 설치하고자 하는 위치에 홈페이지에서 발급 받은 설치코드를 삽입해주세요.</P>
+          </Content>
+
+          <Content>
+            <B>2. 주의 사항</B>
+            <P>스크립트 내부의 코드는 변경해서는 안됩니다.</P>
+          </Content>
+
+          <Content>
+            <B>3. 브라우저 지원 현황</B>
+            <P>
+              다라쓰는 아래의 최신 브라우저 사용을 권장합니다. 구형 브라우저에서는 일부 기능이 동작하지 않을 수
+              있습니다.
+            </P>
+            <P>
+              <ol>
+                <li>Chrome</li>
+                <li>Safari</li>
+                <li>Samsung browser</li>
+              </ol>
+            </P>
+          </Content>
         </Section>
       </Container>
     </ScreenContainer>
