b791fae9ccfb67f394a7e635580d048a7aa20c0d
yungo1846 <yungo1468@naver.com>
2021-07-19 00:38:06 +0900

    feat: 반응형 페이지를 위한 resize event 적용

--- frontend/reply-module/src/App.tsx
+++ frontend/reply-module/src/App.tsx
@@ -1,5 +1,21 @@
 import CommentPage from "./components/pages/CommentPage";
 
+const onResize = () => {
+  let throttle: NodeJS.Timeout | null;
+
+  const postScrollHeightToParent = () => {
+    if (!throttle) {
+      throttle = setTimeout(() => {
+        throttle = null;
+        window.parent.postMessage(document.querySelector("#root")?.scrollHeight, "*");
+      }, 600);
+    }
+  };
+  return postScrollHeightToParent;
+};
+
+window.addEventListener("resize", onResize());
+
 const App = () => {
   return <CommentPage />;
 };
