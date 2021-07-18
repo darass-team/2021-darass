77212afad1583e1e75c7572d7e6e35a75256728a
Sehwan_Jang <68985748+Sehwan-Jang@users.noreply.github.com>
2021-07-18 18:11:03 +0900

    [BE] 서버 시간을 서울로 설정한다 (#180)(#181)

--- backend/src/main/java/com/darass/darass/common/JpaAuditingConfiguration.java
+++ backend/src/main/java/com/darass/darass/common/JpaAuditingConfiguration.java
@@ -1,10 +1,20 @@
 package com.darass.darass.common;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "utcDateTimeProvider")
 public class JpaAuditingConfiguration {
 
+    @Bean
+    public DateTimeProvider utcDateTimeProvider() {
+        return () -> Optional.of(LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+    }
 }
