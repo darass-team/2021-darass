ae7a6924db3cbd745f91c9ca546bf8d6231b8558
jujubebat <bbwwpark@naver.com>
2021-07-20 17:10:53 +0900

    refactor: 카카오에서 받아온 데이터기준으로 사용자 정보 업데이트 하도록 리팩터링

--- backend/src/test/java/com/darass/darass/SpringContainerTest.java
+++ backend/src/test/java/com/darass/darass/SpringContainerTest.java
@@ -8,5 +8,9 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @SpringBootTest
 public class SpringContainerTest extends IsolationTest {
+<<<<<<< HEAD:backend/src/test/java/com/darass/darass/SpringContainerTest.java
+=======
+
+>>>>>>> f87566f (refactor: 카카오에서 받아온 데이터기준으로 사용자 정보 업데이트 하도록 리팩터링):backend/src/test/java/com/darass/darass/DarassApplicationTests.java
 
 }
