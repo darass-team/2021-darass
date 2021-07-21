ce9508cb0b96aeffff0a78dda0c2791f2b92c9fb
zereight <fullstackmachinelearning@gmail.com>
2021-07-21 17:44:54 +0900

    fix: 프로젝트 명 공백 입력 오류 수정
    - autoFocus추가

--- frontend/manage/src/components/pages/NewProjectPage/index.tsx
+++ frontend/manage/src/components/pages/NewProjectPage/index.tsx
@@ -1,23 +1,10 @@
-import { FormEvent } from "react";
-import { useHistory } from "react-router-dom";
-import { ROUTE } from "../../../constants";
-import { useCreateProject, useInput } from "../../../hooks";
+import { useGetAllProjects } from "../../../hooks";
 import NewProject from "../../templates/NewProject";
 
 const NewProjectPage = () => {
-  const history = useHistory();
-  const { createProject } = useCreateProject();
-  const { value: projectName, onChange: onChangeProjectName } = useInput("");
+  const { projects } = useGetAllProjects();
 
-  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const project = await createProject(projectName);
-
-    history.push(ROUTE.GET_SCRIPT_PUBLISHING(project.id));
-  };
-
-  return <NewProject onSubmit={onSubmit} projectName={projectName} onChangeProjectName={onChangeProjectName} />;
+  return <NewProject projects={projects} />;
 };
 
 export default NewProjectPage;
