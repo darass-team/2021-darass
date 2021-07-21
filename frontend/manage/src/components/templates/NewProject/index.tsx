326969246ba3cd6e5d62951e9890207aa845f8e7
zereight <42544600+zereight@users.noreply.github.com>
2021-07-21 18:12:09 +0900

    [FE][관리자 페이지] 프로젝트 생성 공백 문자 입력 예외 처리 (#186) (#240)
    * chore: jest 환경 설정
    
    * feat: 공백 제거 유틸함수 추가 및 테스트코드 작성
    
    * fix: 프로젝트 명 공백 입력 오류 수정
    
    - autoFocus추가
    
    * refactor: ol 태그 외부에있는 p 태그 제거
    
    * refactor: 함수명과 내부 동작 불일치된 부분 수정
    
    * refactor: 어색한 코드 라인 수정 및 프로젝트 이름 중복체크 기능 추가

--- frontend/manage/src/components/templates/NewProject/index.tsx
+++ frontend/manage/src/components/templates/NewProject/index.tsx
@@ -1,22 +1,47 @@
 import { ChangeEvent, FormEvent } from "react";
+import { useCreateProject, useInput } from "../../../hooks";
 import { PALETTE } from "../../../styles/palette";
 import ScreenContainer from "../../../styles/ScreenContainer";
 import { Container, Form, Input, Label, SubmitButton, Title } from "./styles";
+import { useHistory } from "react-router-dom";
+import { isEmptyString } from "../../../utils/validation";
+import { ROUTE } from "../../../constants";
+import { Project } from "../../../types/project";
 
 export interface Props {
-  projectName: string;
-  onChangeProjectName: (event: ChangeEvent<HTMLInputElement>) => void;
-  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  projects: Project[] | undefined;
 }
 
-const NewProject = ({ onSubmit, projectName, onChangeProjectName }: Props) => {
+const NewProject = ({ projects }: Props) => {
+  const history = useHistory();
+  const { createProject } = useCreateProject();
+  const { value: projectName, onChange: onChangeProjectName } = useInput("");
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const isDuplicatedProjectName = projects?.some(project => project.name === projectName.trim());
+
+      if (isEmptyString(projectName)) throw new Error("프로젝트 이름을 입력해주세요.");
+      if (isDuplicatedProjectName) throw new Error("중복된 프로젝트 이름입니다. 다시 입력해주세요.");
+
+      const project = await createProject(projectName.trim());
+
+      history.push(ROUTE.GET_SCRIPT_PUBLISHING(project.id));
+    } catch (error) {
+      alert(error.message);
+      console.error(error.message);
+    }
+  };
+
   return (
     <ScreenContainer>
       <Container>
         <Title>새 프로젝트 만들기</Title>
         <Form onSubmit={onSubmit}>
           <Label htmlFor="project-name">프로젝트 이름</Label>
-          <Input id="project-name" value={projectName} onChange={onChangeProjectName} />
+          <Input id="project-name" value={projectName} onChange={onChangeProjectName} autoFocus />
           <SubmitButton>등록</SubmitButton>
         </Form>
       </Container>
