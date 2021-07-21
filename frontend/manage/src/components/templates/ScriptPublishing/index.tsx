0c7957cd0b6af2de48c610f5b6a667b565bc3e8b
yungo1846 <yungo1468@naver.com>
2021-07-21 22:35:03 +0900

    feat: script 코드 상세 가이드 추가
    Co-authored-by: zereight <zereight@users.noreply.github.com>

--- frontend/manage/src/components/templates/ScriptPublishing/index.tsx
+++ frontend/manage/src/components/templates/ScriptPublishing/index.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { xcode } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { GUIDE_FILE } from "../../../constants";
 import { useCopyButton } from "../../../hooks";
 import ScreenContainer from "../../../styles/ScreenContainer";
-import { B, CodeBlockWrapper, Container, Content, CopyButton, P, Ol, Section, Title } from "./styles";
+import {
+  B,
+  CodeBlockWrapper,
+  Container,
+  Content,
+  CopyButton,
+  P,
+  Ol,
+  Section,
+  Title,
+  BlogLogoWrapper,
+  BlogLogo
+} from "./styles";
 
 const scriptCode = (projectSecretKey: string) => `
 <!-- 다라쓰 설치 코드 -->
@@ -28,8 +42,9 @@ export interface Props {
 }
 
 const ScriptPublishing = ({ projectSecretKey }: Props) => {
-  const script = scriptCode(projectSecretKey || "코드를 불러오는 중입니다...");
+  const [selectedBlogInfo, setSelectedBlogInfo] = useState(GUIDE_FILE.UNIVERSAL);
   const { isCopyButtonClicked, onCopy } = useCopyButton();
+  const script = scriptCode(projectSecretKey || "코드를 불러오는 중입니다...");
 
   return (
     <ScreenContainer>
@@ -49,9 +64,17 @@ const ScriptPublishing = ({ projectSecretKey }: Props) => {
         <Section>
           <Title>스크립트 적용 가이드</Title>
 
+          <BlogLogoWrapper>
+            {console.log()}
+            {Object.entries(GUIDE_FILE).map(([blogName, info]) => (
+              <BlogLogo src={info.logoURL} alt={blogName} key={blogName} onClick={() => setSelectedBlogInfo(info)} />
+            ))}
+          </BlogLogoWrapper>
+
           <Content>
             <B>1. 다라쓰 코드 설치</B>
             <P> 다라쓰를 설치하고자 하는 위치에 홈페이지에서 발급 받은 설치코드를 삽입해주세요.</P>
+            <iframe src={selectedBlogInfo.iframeSrc} style={{ width: "100%", height: "700px" }} frameBorder="0" />
           </Content>
 
           <Content>
