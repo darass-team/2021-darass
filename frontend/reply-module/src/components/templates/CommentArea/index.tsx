219aa138bf4618d5280105e7dae7ef1b859a90bf
zereight <fullstackmachinelearning@gmail.com>
2021-07-22 17:28:45 +0900

    refactor: 댓글이 없을때 댓글 정렬, 목록 영역 노출안되도록 변경

--- frontend/reply-module/src/components/templates/CommentArea/index.tsx
+++ frontend/reply-module/src/components/templates/CommentArea/index.tsx
@@ -34,7 +34,7 @@ const CommentArea = ({ user, comments = [], onLogin, onLogout, projectSecretKey,
         </UserAvatarOption>
       </Header>
       <CommentInput url={url} projectSecretKey={projectSecretKey} user={user} />
-      <CommentList user={user} comments={comments} />
+      {comments.length > 0 && <CommentList user={user} comments={comments} />}
     </Container>
   );
 };
