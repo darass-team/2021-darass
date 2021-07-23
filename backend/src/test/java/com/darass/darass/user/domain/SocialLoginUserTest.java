4b24d5a6bbb525e15b5b5773ce715c39dd0794f3
jujubebat <bbwwpark@naver.com>
2021-07-23 20:30:05 +0900

    feat: SocialLoginUser, GuestUserTest 단위 테스트 작성

--- backend/src/test/java/com/darass/darass/user/domain/SocialLoginUserTest.java
+++ backend/src/test/java/com/darass/darass/user/domain/SocialLoginUserTest.java
@@ -1,8 +1,11 @@
 package com.darass.darass.user.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.darass.darass.auth.oauth.api.domain.OAuthProviderType;
+import com.darass.darass.exception.ExceptionWithMessageAndCode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,17 +16,21 @@ class SocialLoginUserTest {
     private final String email = "bbwwpark@naver.com";
     private final String oauthId = "12314341451354";
     private final OAuthProviderType oAuthProviderType = OAuthProviderType.KAKAO;
+    private SocialLoginUser socialLoginUser;
 
-    @DisplayName("SocialLoginUser 객체가 빌더 패턴으로 생성된다.")
-    @Test
-    void constructor() {
-        SocialLoginUser socialLoginUser = SocialLoginUser.builder()
+    @BeforeEach
+    void setUp(){
+        socialLoginUser = SocialLoginUser.builder()
             .nickName(nickName)
             .email(email)
             .oauthId(oauthId)
             .oauthProviderType(oAuthProviderType)
             .build();
+    }
 
+    @DisplayName("SocialLoginUser 객체가 빌더 패턴으로 생성된다.")
+    @Test
+    void constructor() {
         assertThat(socialLoginUser).isNotNull();
         assertThat(socialLoginUser.getNickName()).isEqualTo(nickName);
         assertThat(socialLoginUser.getOauthId()).isEqualTo(oauthId);
@@ -33,14 +40,14 @@ class SocialLoginUserTest {
     @DisplayName("isLoginUser 메서드는 true를 리턴한다.")
     @Test
     void isLoginUser() {
-        SocialLoginUser socialLoginUser = SocialLoginUser.builder()
-            .nickName(nickName)
-            .email(email)
-            .oauthId(oauthId)
-            .oauthProviderType(oAuthProviderType)
-            .build();
-
         assertThat(socialLoginUser.isLoginUser()).isTrue();
     }
 
+    @DisplayName("isValidGuestPassword 메서드는 패스워드가 주어다면, 예외를 던진다.")
+    @Test
+    void isValidGuestPassword() {
+        assertThatThrownBy(() -> {
+            assertThat(socialLoginUser.isValidGuestPassword("password")).isTrue();
+        }).isEqualTo(ExceptionWithMessageAndCode.NOT_GUEST_USER.getException());
+    }
 }
