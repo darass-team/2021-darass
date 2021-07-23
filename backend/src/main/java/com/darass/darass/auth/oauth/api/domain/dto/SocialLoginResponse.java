47acabe8b5e09792b74068fd1a4a7519c76144e1
Sehwan_Jang <68985748+Sehwan-Jang@users.noreply.github.com>
2021-07-23 16:23:55 +0900

    [BE] Github, Naver 소셜 로그인 추가 (#216) (#262)
    * feat: OAuthProvider 추상화
    
    * feat: oAuth 추상화
    
    * feat: github oauth 구현
    
    * feat: naver oauth 구현
    
    * refactor: 플랫폼 타입 변경

--- backend/src/main/java/com/darass/darass/auth/oauth/api/domain/dto/SocialLoginResponse.java
+++ backend/src/main/java/com/darass/darass/auth/oauth/api/domain/dto/SocialLoginResponse.java
@@ -1,17 +1,12 @@
 package com.darass.darass.auth.oauth.api.domain.dto;
 
+import com.darass.darass.user.domain.SocialLoginUser;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-public class SocialLoginResponse {
+public interface SocialLoginResponse {
 
-    private String id;
-
-    @JsonProperty("kakao_account")
-    private KaKaoAccount kaKaoAccount;
+    SocialLoginUser toEntity();
 }
