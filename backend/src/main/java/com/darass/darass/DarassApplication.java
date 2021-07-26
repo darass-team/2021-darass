b610119d88a11489727c3785f6d9ba4ef11c5817
jujubebat <bbwwpark@naver.com>
2021-07-27 03:30:59 +0900

    refactor: 수정1

--- backend/src/main/java/com/darass/darass/DarassApplication.java
+++ backend/src/main/java/com/darass/darass/DarassApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DarassApplication {
 
     public static void main(String[] args) {
+        System.out.println("수정1");
         SpringApplication.run(DarassApplication.class, args);
     }
 }
