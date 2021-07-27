225571bd9ec16329ed15edd59c56bcda4849a33b
zereight <fullstackmachinelearning@gmail.com>
2021-07-27 20:27:59 +0900

    refactor: 공통 스타일 요소 분리

--- frontend/manage/src/components/pages/UserProfile/index.tsx
+++ frontend/manage/src/components/pages/UserProfile/index.tsx
@@ -2,18 +2,8 @@ import { ChangeEvent, FormEventHandler, useEffect, useState } from "react";
 import cameraIcon from "../../../assets/svg/camera.svg";
 import { useEditUser, useInput, useUser } from "../../../hooks";
 import ScreenContainer from "../../../styles/ScreenContainer";
-import {
-  CameraIcon,
-  Container,
-  FileLabel,
-  Form,
-  InfoWrapper,
-  Input,
-  Label,
-  SubmitButton,
-  Title,
-  UserProfileImage
-} from "./styles";
+import SubmitButton from "../../atoms/Buttons/SubmitButton";
+import { CameraIcon, Container, FileLabel, Form, InfoWrapper, Input, Label, Title, UserProfileImage } from "./styles";
 
 const UserProfile = () => {
   const { user } = useUser();
@@ -54,7 +44,6 @@ const UserProfile = () => {
           <Title>프로필</Title>
 
           <InfoWrapper>
-            <Label>사진</Label>
             <FileLabel>
               <CameraIcon src={cameraIcon} />
               <UserProfileImage imageURL={profileImage} size="LG" />
--- frontend/manage/src/components/pages/UserProfile/styles.ts
+++ frontend/manage/src/components/pages/UserProfile/styles.ts
@@ -1,20 +1,15 @@
 import styled from "styled-components";
-import { PALETTE } from "../../../styles/palette";
+import { contentBoxCSS, inputCSS, labelCSS, titleCSS } from "../../../styles/css";
 import Avatar from "../../atoms/Avatar";
-import SubmitButtonComponent from "../../atoms/SubmitButton";
 
 export const Container = styled.div`
-  width: 45rem;
-  padding: 6rem 5.5rem;
-  background-color: ${PALETTE.WHITE};
-  border-radius: 15px;
-  box-shadow: rgba(0, 0, 0, 0.24) 0 3px 8px;
+  width: 40rem;
+  ${contentBoxCSS}
 `;
 
 export const Title = styled.h2`
-  font-size: 1.6rem;
-  font-weight: 800;
-  margin-bottom: 3rem;
+  ${titleCSS}
+  margin-bottom: 5rem;
   align-self: flex-start;
 `;
 
@@ -38,19 +33,12 @@ export const UserProfileImage = styled(Avatar)`
 `;
 
 export const Label = styled.label`
-  font-size: 1.4rem;
-  font-weight: 600;
-  align-self: flex-start;
-  margin-bottom: 0.8rem;
+  ${labelCSS}
 `;
 
 export const CameraIcon = styled.img`
-  background-position: 0px -46px;
-  background-size: 25px 207px;
   width: 30px;
   height: 30px;
-  background-repeat: no-repeat;
-  display: inline-block;
   cursor: pointer;
   position: absolute;
   right: 0;
@@ -67,20 +55,5 @@ export const FileLabel = styled(Label)`
 `;
 
 export const Input = styled.input`
-  width: 100%;
-  height: 3rem;
-  border: 1px solid ${PALETTE.BLACK_700};
-  border-radius: 10px;
-  font-size: 1.2rem;
-  padding: 0.7rem 1rem;
-
-  &:focus {
-    outline: none;
-    box-shadow: 0 0 0 2px ${PALETTE.BLACK_700};
-  }
-`;
-
-export const SubmitButton = styled(SubmitButtonComponent)`
-  background-color: ${PALETTE.GRAY_300};
-  color: ${PALETTE.BLACK_700};
+  ${inputCSS}
 `;
