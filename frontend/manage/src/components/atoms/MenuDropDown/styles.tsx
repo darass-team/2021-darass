5a4d4f9dee8ae0cbf2e42c2bd0fefd865cacdcff
zereight <fullstackmachinelearning@gmail.com>
2021-07-30 14:52:35 +0900

    refactor: depth에 따른 DropDownMenu z-index 조절

--- frontend/manage/src/components/atoms/MenuDropDown/styles.tsx
+++ frontend/manage/src/components/atoms/MenuDropDown/styles.tsx
@@ -9,22 +9,15 @@ export const Container = styled.div<{ isDropDown: boolean | null }>`
   @keyframes fadein {
     0% {
       transform: translateY(-100%);
-
       opacity: 0;
-      z-index: -1;
       max-height: 0;
     }
-    30% {
+    70% {
       max-height: 100vh;
-    }
-    90% {
-      z-index: -1;
     }
     100% {
       transform: translateY(0%);
-
       opacity: 1;
-      z-index: 1;
       max-height: 100vh;
     }
   }
@@ -32,26 +25,19 @@ export const Container = styled.div<{ isDropDown: boolean | null }>`
   @keyframes fadeout {
     0% {
       transform: translateY(0%);
-
       opacity: 1;
-      z-index: 1;
       max-height: 100vh;
     }
-    10% {
-      z-index: -1;
-    }
+
     70% {
       max-height: 0;
     }
     100% {
       transform: translateY(-100%);
-
       opacity: 0;
-      z-index: -1;
       max-height: 0;
     }
   }
-  overflow: hidden;
 
   > *:not(:first-child) {
     overflow: hidden;
@@ -62,6 +48,7 @@ export const Container = styled.div<{ isDropDown: boolean | null }>`
 `;
 
 export const MainTitle = styled.button<{ isDropDown: boolean | null; depth: number }>`
+  overflow: hidden;
   position: relative;
   text-align: left;
   display: flex;
@@ -70,7 +57,7 @@ export const MainTitle = styled.button<{ isDropDown: boolean | null; depth: numb
   font-weight: 700;
   font-size: ${props => 1.2 - 1.2 * 0.1 * props.depth}rem;
   padding: 0.3rem 0 0.3rem ${props => 1 + 1 * 0.5 * props.depth}rem;
-
+  z-index: ${props => 1 + props.depth};
   transition: all 0.1s ease-in;
 
   &:hover {
