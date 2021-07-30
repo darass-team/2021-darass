fdf5767f8aa35a562e7d97a9e627f5ce4c3155cd
Byung Wook Park <bbwwpark@naver.com>
2021-07-30 13:49:06 +0900

    fix: 외래키 제약 조건으로인해 유저가 삭제 되지 않는 버그 해결 (#312)

--- backend/src/main/java/com/darass/darass/comment/repository/CommentLikeRepository.java
+++ backend/src/main/java/com/darass/darass/comment/repository/CommentLikeRepository.java
@@ -3,6 +3,6 @@ package com.darass.darass.comment.repository;
 import com.darass.darass.comment.domain.CommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LikeRepository extends JpaRepository<CommentLike, Long> {
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 
 }
