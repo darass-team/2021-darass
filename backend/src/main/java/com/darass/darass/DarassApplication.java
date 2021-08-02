a5b98d4bb44ff76e1665b2f5847c062ede1ba700
jujubebat <bbwwpark@naver.com>
2021-08-02 17:08:36 +0900

    수정 14

--- backend/src/main/java/com/darass/darass/DarassApplication.java
+++ backend/src/main/java/com/darass/darass/DarassApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DarassApplication {
 
     public static void main(String[] args) {
-        System.out.println("수정13");
+        System.out.println("수정14");
         SpringApplication.run(DarassApplication.class, args);
     }
 }
