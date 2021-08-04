460d3245f56d372c01beed01721302acfcac2a6d
jujubebat <bbwwpark@naver.com>
2021-08-04 22:41:34 +0900

    test : 젠킨스 테스트

--- backend/src/main/java/com/darass/darass/DarassApplication.java
+++ backend/src/main/java/com/darass/darass/DarassApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DarassApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DarassApplication.class, args);
     }
 }
