bcab0d7dd2cd4193072d8243c11a28d919e810f5
Aaron <whilter08@gmail.com>
2021-08-04 17:29:59 +0900

    refactor: 애플리케이션 테스트 제거 및 자코코 설정 변경

--- backend/src/main/java/com/darass/darass/DarassApplication.java
+++ backend/src/main/java/com/darass/darass/DarassApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DarassApplication {
 
-    @Generated
     public static void main(String[] args) {
         SpringApplication.run(DarassApplication.class, args);
     }
