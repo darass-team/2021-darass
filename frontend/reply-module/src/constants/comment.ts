410423ef9cccf40f59bc0aaa802ee528317a2a22
Kiyung Kim <yungo1468@naver.com>
2021-08-06 23:48:15 +0900

    [FE][댓글모듈] 댓글 입력 예외 처리 (#187) (#414)
    * refactor: 좋아요, 더보기 에러 메세지 변경
    
    * feat: 댓글 입력 에러 메세지 구체화

--- frontend/reply-module/src/constants/comment.ts
+++ frontend/reply-module/src/constants/comment.ts
@@ -1,2 +1,7 @@
 export const NUM_OF_COMMENTS_PER_PAGE = 5;
 export const INITIAL_PAGE_PARAM = 1;
+export const MAX_COMMENT_INPUT_LENGTH = 1000;
+export const GUEST_NICKNAME_MIN_LENGTH = 1;
+export const GUEST_NICKNAME_MAX_LENGTH = 20;
+export const GUEST_PASSWORD_MIN_LENGTH = 1;
+export const GUEST_PASSWORD_MAX_LENGTH = 20;
--- frontend/reply-module/src/styles/css.ts
+++ frontend/reply-module/src/styles/css.ts
@@ -4,7 +4,7 @@ import { PALETTE } from "./palette";
 export const InputCSS = css`
   box-sizing: border-box;
   border: 1px solid ${PALETTE.GRAY_500};
-  border-radius: 0.3rem;
+  border-radius: 10px;
   color: ${PALETTE.BLACK_700};
   font-size: 1.2rem;
   line-height: 1.8rem;
