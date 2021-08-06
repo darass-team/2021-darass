cdc2492326b2084bb690b62fb9b767786a889edd
Aaron <whilter08@gmail.com>
2021-08-06 11:15:25 +0900

    chore: flyway plugin 추가

--- backend/src/test/resources/testdb/migration/V20210805161911__add_sub_comment.sql
+++ backend/src/test/resources/testdb/migration/V20210805161911__add_sub_comment.sql
@@ -3,4 +3,5 @@ alter table comment add parent_id bigint;
 alter table comment
     add constraint comment_fk_sub_comment
         foreign key (parent_id)
-            references comment (id);
+            references comment (id)
+            on delete cascade;
--- backend/src/test/resources/testdb/migration/V20210805183839__cascade_sub_comment.sql
+++
@@ -1,7 +0,0 @@
-alter table comment drop foreign key comment_fk_sub_comment;
-
-alter table comment
-    add constraint comment_fk_sub_comment
-        foreign key (parent_id)
-            references comment (id)
-            on delete cascade;
