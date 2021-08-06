01bf9aea752288e03c07534e6b886ebd2b3b0320
JinYoung Park <basejin0810@naver.com>
2021-08-06 17:22:40 +0900

    refactor: 메소드명 수정

--- backend/src/main/java/com/darass/darass/comment/controller/CommentController.java
+++ backend/src/main/java/com/darass/darass/comment/controller/CommentController.java
@@ -58,7 +58,7 @@ public class CommentController {
     }
 
     @GetMapping("/projects/comments/paging")
-    public ResponseEntity<List<CommentReadResponseInProject>> readByPageRequest(
+    public ResponseEntity<List<CommentReadResponseInProject>> readByPageRequestInProject(
         @ModelAttribute CommentReadRequestInProject commentReadRequestInProject) {
         List<CommentReadResponseInProject> commentResponses = commentService
             .findAllCommentsInProject(commentReadRequestInProject);
