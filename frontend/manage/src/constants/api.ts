5532d614c5d45c5c3cff0308d26aee2f15230847
zereight <fullstackmachinelearning@gmail.com>
2021-08-09 14:41:28 +0900

    refactor: login api 변경사항 적용

--- frontend/manage/src/constants/api.ts
+++ frontend/manage/src/constants/api.ts
@@ -2,7 +2,7 @@ const PRODUCTION_BASE_URL = "https://darass.o-r.kr";
 const DEVELOPMENT_BASE_URL = "https://darass-develop.o-r.kr";
 const BASE_URL = process.env.BUILD_MODE === "development" ? PRODUCTION_BASE_URL : PRODUCTION_BASE_URL;
 const QUERY = {
-  LOGIN: "/api/v1/login/oauth?oauthProviderName=kakao&oauthAccessToken=",
+  LOGIN: "/api/v1/login/oauth",
   USER: "/api/v1/users",
   COMMENT: "/api/v1/comments",
   PROJECT: "/api/v1/projects",
