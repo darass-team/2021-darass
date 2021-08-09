2c6bb7052a2b208e045973e0c6b0fe5e76555226
JinYoung Park <basejin0810@naver.com>
2021-08-09 14:20:37 +0900

    chore: 파라미터명 수정

--- backend/src/main/java/com/darass/darass/comment/repository/CommentCountStrategyFactory.java
+++ backend/src/main/java/com/darass/darass/comment/repository/CommentCountStrategyFactory.java
@@ -12,9 +12,9 @@ public class CommentCountStrategyFactory {
 
     private final List<CommentCountStrategy> commentCountStrategies;
 
-    public CommentCountStrategy findStrategy(String period) {
+    public CommentCountStrategy findStrategy(String periodicity) {
         return commentCountStrategies.stream()
-            .filter(strategy -> !Objects.isNull(period) && strategy.isCountable(period))
+            .filter(strategy -> !Objects.isNull(periodicity) && strategy.isCountable(periodicity))
             .findAny()
             .orElseThrow(ExceptionWithMessageAndCode.NOT_FOUND_PERIODICITY::getException);
     }
