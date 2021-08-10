29ba421d884d0af6db20ad56f038674ad707b14c
jaeseongDev <qkrwotjd1445@naver.com>
2021-08-10 09:48:17 +0900

    feat: cors 설정 수정

--- backend/src/main/java/com/darass/darass/WebConfig.java
+++ backend/src/main/java/com/darass/darass/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
+            .allowCredentials(true)
             .allowedMethods("*");
     }
 }
