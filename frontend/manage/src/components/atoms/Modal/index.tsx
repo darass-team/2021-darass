c1cb38516ede85f255fe97f551b7389c195504dc
zereight <fullstackmachinelearning@gmail.com>
2021-08-27 16:22:57 +0900

    fix: z-index 차이를 늘려서 딤드 지연현상 해결 및 Modal에 스크롤 막는 인자 추가

--- frontend/manage/src/components/atoms/Modal/index.tsx
+++ frontend/manage/src/components/atoms/Modal/index.tsx
@@ -5,10 +5,11 @@ export interface Props {
   isOpen: boolean;
   children: ReactNode;
   dimmedOpacity?: number;
+  blockScroll?: boolean;
   closeModal: () => void;
 }
 
-const Modal = ({ isOpen, closeModal, children, dimmedOpacity = 0.6 }: Props) => {
+const Modal = ({ isOpen, closeModal, children, dimmedOpacity = 0.6, blockScroll = true }: Props) => {
   const dimmedRef = useRef(null);
   const onCloseModal = (event: MouseEvent) => {
     if (event.target !== dimmedRef.current) return;
@@ -17,13 +18,13 @@ const Modal = ({ isOpen, closeModal, children, dimmedOpacity = 0.6 }: Props) => 
   };
 
   useEffect(() => {
-    document.body.style.setProperty("overflow", isOpen ? "hidden" : "revert");
+    if (blockScroll) document.body.style.setProperty("overflow", isOpen ? "hidden" : "revert");
   }, [isOpen]);
 
   return (
     <>
       <Dimmed ref={dimmedRef} onClick={onCloseModal} isOpen={isOpen} opacity={dimmedOpacity} />
-      {isOpen && children}
+      {children}
     </>
   );
 };
