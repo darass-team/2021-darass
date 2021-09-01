04ed9e0f79707fd0682998167062a2565ce29c49
yungo1846 <yungo1468@naver.com>
2021-09-01 15:55:13 +0900

    feat: URL의 마지막 문자가 '/'인 경우, '/' 삭제

--- frontend/deploy-script/src/getURL.js
+++ frontend/deploy-script/src/getURL.js
@@ -3,7 +3,11 @@ import { END_POINT } from "./constants.js";
 export const getReplyModuleURL = () => {
   const $darass = document.querySelector("#darass");
   const replyModuleURL = END_POINT + "?";
-  const currentURL = window.location.origin + window.location.pathname;
+
+  let currentURL = window.location.origin + window.location.pathname;
+  if (currentURL.endsWith("/")) {
+    currentURL = currentURL.slice(0, -1);
+  }
   const projectKey = $darass.dataset.projectKey;
 
   const urlParams = new URLSearchParams(replyModuleURL);
