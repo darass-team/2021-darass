1fdd9eda699cde006d9c6d332257a1d337a082ee
Byung Wook Park <bbwwpark@naver.com>
2021-09-08 14:43:16 +0900

    refactor: 카카오 api 서버에서 유저 이메일 정보를 제공하지 않을 경우에도 로그인 가능하도록 리팩터링

--- backend/src/test/java/com/darass/darass/auth/oauth/api/domain/KaKaoOAuthProviderTest.java
+++ backend/src/test/java/com/darass/darass/auth/oauth/api/domain/KaKaoOAuthProviderTest.java
@@ -80,4 +80,32 @@ class KaKaoOAuthProviderTest {
         assertThatThrownBy(() -> kaKaoOAuthProvider.requestSocialLoginUser("authorizationCode"))
             .isInstanceOf(ExceptionWithMessageAndCode.INVALID_OAUTH_ACCESS_TOKEN.getException().getClass());
     }
+
+    @DisplayName("findSocialLoginUser 메서드는 카카오 인증서버에 인가 코드를 전송해서 엑세스 토큰을 얻어와 카카오 api 서버에 보낸후 SocialLoginUser 객체를 받아온다.(카카오에서 유저 이메일 정보를 제공하지 않는경우)")
+    @Test
+    void findSocialLoginUser_not_provide_userEmail() {
+        String userInfoResponse =
+            "{\"id\":1791380911,\"connected_at\":\"2021-07-02T10:42:36Z\",\"properties\":{\"nickname\":\"병욱\"},"
+                + "\"kakao_account\":{\"profile_nickname_needs_agreement\":false,\"profile_image_needs_agreement\":false,\""
+                + "profile\":{\"nickname\":\"병욱\",\"thumbnail_image_url\":\"http://k.kakaocdn.net/dn/cQc7At/btq9Zq3eBJb/DDAc7l5Iv7rOJfF7DZZkfK/img_110x110.jpg\","
+                + "\"profile_image_url\":\"http://k.kakaocdn.net/dn/cQc7At/btq9Zq3eBJb/DDAc7l5Iv7rOJfF7DZZkfK/img_640x640.jpg\",\"is_default_image\":false},\"has_email\":true,"
+                + "\"email_needs_agreement\":false,\"is_email_valid\":true,\"is_email_verified\":true,\"has_age_range\":true,"
+                + "\"age_range_needs_agreement\":false,\"age_range\":\"20~29\",\"has_birthday\":true,\"birthday_needs_agreement\":false,\"birthday\":\"1114\",\"birthday_type\":\"SOLAR\","
+                + "\"has_gender\":true,\"gender_needs_agreement\":false,\"gender\":\"male\"}}";
+
+
+        mockServer.expect(requestTo(authorizationServerUrl))
+            .andRespond(withSuccess("{\"access_token\":\"kakaoAccessToken\"}", MediaType.APPLICATION_JSON));
+        mockServer.expect(requestTo(apiServerUrl))
+            .andRespond(withSuccess(userInfoResponse, MediaType.APPLICATION_JSON));
+
+        //then
+        SocialLoginUser socialLoginUser = kaKaoOAuthProvider.requestSocialLoginUser("authorizationCode");
+
+        //when
+        assertThat(socialLoginUser.getNickName()).isEqualTo("병욱");
+        assertThat(socialLoginUser.getEmail()).isEqualTo(null);
+        assertThat(socialLoginUser.getProfileImageUrl())
+            .isEqualTo("http://k.kakaocdn.net/dn/cQc7At/btq9Zq3eBJb/DDAc7l5Iv7rOJfF7DZZkfK/img_110x110.jpg");
+    }
 }
