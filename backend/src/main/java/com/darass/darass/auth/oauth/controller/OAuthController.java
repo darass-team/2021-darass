28cb912d3f996abc274e2926f1848ff921afd897
Byung Wook Park <bbwwpark@naver.com>
2021-09-08 15:09:55 +0900

    refactor: 쿠키 설정 수정

--- backend/src/main/java/com/darass/darass/auth/oauth/controller/OAuthController.java
+++ backend/src/main/java/com/darass/darass/auth/oauth/controller/OAuthController.java
@@ -49,10 +49,10 @@ public class OAuthController {
 
     private void createCookie(HttpServletResponse response, String refreshToken) {
         ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN_NAME, refreshToken)
-            .sameSite("None")
+            .sameSite("Lax")
             .maxAge(SECONDS_OF_TWO_MONTHS)
             .path("/")
-            .secure(true)
+            .httpOnly(true)
             .build();
         response.addHeader("Set-Cookie", cookie.toString());
     }
