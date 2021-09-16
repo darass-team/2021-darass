8faba83631d54c3894a626a85f542ad7f1beaec6
yungo1846 <yungo1468@naver.com>
2021-09-16 11:30:41 +0900

    feat: token refetch timer가 build mode에 따라 다르게 설정되도록 변경

--- frontend/manage/src/constants/timer.ts
+++ frontend/manage/src/constants/timer.ts
@@ -1,1 +1,9 @@
-export const TOKEN_REFETCH_TIMER = 25 * 1000;
+import { BUILD_MODE_TABLE } from "../types/buildMode";
+
+const TOKEN_REFETCH_TIMER_TABLE = {
+  localhost: 25 * 1000,
+  development: 25 * 1000,
+  production: 55 * 60 * 1000
+};
+
+export const TOKEN_REFETCH_TIMER = TOKEN_REFETCH_TIMER_TABLE[process.env.BUILD_MODE as keyof BUILD_MODE_TABLE];
