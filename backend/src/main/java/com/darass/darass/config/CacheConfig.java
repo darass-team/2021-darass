b097b52e805d86d507f6036aa38c94a4bc00b45b
Byung Wook Park <bbwwpark@naver.com>
2021-09-20 22:40:51 +0900

    chore: 테스트 수행시 캐싱 적용 안되도록 수정

--- backend/src/main/java/com/darass/darass/config/CacheConfig.java
+++ backend/src/main/java/com/darass/darass/config/CacheConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Profile("!test")
 @EnableCaching
 @Configuration
 public class CacheConfig extends CachingConfigurerSupport {
