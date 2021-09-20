fff733b22865f920c53244fb75a87c6188c9515c
Byung Wook Park <bbwwpark@naver.com>
2021-09-20 22:01:10 +0900

    feat: 댓글 조회 서비스 메서드에 캐싱 적용

--- backend/src/main/java/com/darass/darass/user/dto/UserResponse.java
+++ backend/src/main/java/com/darass/darass/user/dto/UserResponse.java
@@ -2,7 +2,9 @@ package com.darass.darass.user.dto;
 
 import com.darass.darass.user.domain.User;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,9 +24,11 @@ public class UserResponse {
     private String profileImageUrl;
 
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime createdDate;
 
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime modifiedDate;
 
     public static UserResponse of(User user, String type) {
