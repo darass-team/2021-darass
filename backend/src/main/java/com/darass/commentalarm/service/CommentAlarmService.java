6a60b4e2f9794be3910231a74d93f39f56b3373d
Byung Wook Park <bbwwpark@naver.com>
2021-09-28 13:57:10 +0900

    웹 소켓으로 보낼 알람 데이터를 json으로 변환할때 문한 루프가 발생하는 버그를 해결한다.  (#605)

--- backend/src/main/java/com/darass/commentalarm/service/CommentAlarmService.java
+++ backend/src/main/java/com/darass/commentalarm/service/CommentAlarmService.java
@@ -1,12 +1,9 @@
 package com.darass.commentalarm.service;
 
-import com.darass.comment.dto.CommentResponse;
 import com.darass.commentalarm.domain.CommentAlarm;
-import com.darass.commentalarm.domain.CommentAlarmType;
 import com.darass.commentalarm.dto.CommentAlarmResponse;
 import com.darass.commentalarm.repository.CommentAlarmRepository;
 import com.darass.user.domain.SocialLoginUser;
-import com.darass.user.dto.UserResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,20 +18,17 @@ public class CommentAlarmService {
 
     private final CommentAlarmRepository commentAlarmRepository;
 
-    public List<CommentAlarmResponse> findAllBySenderAndCreatedDateBetween(SocialLoginUser socialLoginUser, LocalDateTime start,
+    public List<CommentAlarmResponse> findAllBySenderAndCreatedDateBetween(SocialLoginUser socialLoginUser,
+        LocalDateTime start,
         LocalDateTime end) {
-        List<CommentAlarm> commentAlarms = commentAlarmRepository.findAllBySenderAndCreatedDateBetween(socialLoginUser, start, end);
+        List<CommentAlarm> commentAlarms = commentAlarmRepository
+            .findAllBySenderAndCreatedDateBetween(socialLoginUser, start, end);
 
         return makeCommentAlarmResponses(commentAlarms);
     }
 
     private List<CommentAlarmResponse> makeCommentAlarmResponses(List<CommentAlarm> commentAlarms) {
-        return commentAlarms.stream().map(commentAlarm -> {
-            CommentAlarmType commentAlarmType = commentAlarm.getCommentAlarmType();
-            UserResponse userResponse = UserResponse.of(commentAlarm.getSender());
-            CommentResponse commentResponse = CommentResponse.of(commentAlarm.getComment(), userResponse);
-            return CommentAlarmResponse.of(commentAlarmType, userResponse, commentResponse);
-        }).collect(Collectors.toList());
+        return commentAlarms.stream().map(CommentAlarm::makeCommentAlarmResponse).collect(Collectors.toList());
     }
 
 }
