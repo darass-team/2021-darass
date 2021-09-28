9144541ca41e6a95bf6041ca8ee70d3a6bdf80e3
Byung Wook Park <bbwwpark@naver.com>
2021-09-28 13:57:10 +0900

    웹 소켓으로 보낼 알람 데이터를 json으로 변환할때 문한 루프가 발생하는 버그를 해결한다.  (#605)

--- backend/src/main/java/com/darass/commentalarm/domain/CommentAlarmMachine.java
+++ backend/src/main/java/com/darass/commentalarm/domain/CommentAlarmMachine.java
@@ -1,5 +1,6 @@
 package com.darass.commentalarm.domain;
 
+import com.darass.commentalarm.dto.CommentAlarmResponse;
 import com.darass.user.domain.User;
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -16,9 +17,10 @@ public class CommentAlarmMachine {
 
     public void sendMessage(User receiver, CommentAlarm commentAlarm) {
         receiver.changeHasRecentAlarm(true);
+        CommentAlarmResponse commentAlarmResponse = commentAlarm.makeCommentAlarmResponse();
 
-        simpMessageSendingOperations.convertAndSend(QUEUE_MAIN + receiver.getId(), commentAlarm);
-        simpMessageSendingOperations.convertAndSend(QUEUE_MODULE + receiver.getId(), commentAlarm);
+        simpMessageSendingOperations.convertAndSend(QUEUE_MAIN + receiver.getId(), commentAlarmResponse);
+        simpMessageSendingOperations.convertAndSend(QUEUE_MODULE + receiver.getId(), commentAlarmResponse);
     }
 
 }
