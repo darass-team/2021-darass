d48972f8e15231f51486c6769a7880b993d80a5a
zereight <fullstackmachinelearning@gmail.com>
2021-10-05 19:57:41 +0900

    refactor: useCommentArea 커스텀 훅 분리

--- frontend/reply-module/src/components/pages/CommentArea/index.tsx
+++ frontend/reply-module/src/components/pages/CommentArea/index.tsx
@@ -1,15 +1,10 @@
-import { useToken } from "@/hooks/api/token/useToken";
-import { useContext, useEffect, useState } from "react";
 import kakaoTalkIcon from "@/assets/png/kakaotalk.png";
 import naverIcon from "@/assets/png/naver.png";
-import { MANAGE_PAGE_DOMAIN } from "@/constants/domain";
-import { OAUTH_URL } from "@/constants/oauth";
-import { ORDER_BUTTON } from "@/constants/orderButton";
-import { useGetAllComments, useGetProjectOwnerId, useMessageChannelFromReplyModuleContext, useUser } from "@/hooks";
-import { AlertError } from "@/utils/alertError";
-import { popUpCenter } from "@/utils/popUpCenter";
 import Avatar from "@/components/atoms/Avatar";
+import CommentInput from "@/components/molecules/CommentInput";
 import Footer from "@/components/molecules/Footer";
+import { MANAGE_PAGE_DOMAIN } from "@/constants/domain";
+import LoadingPage from "../LoadingPage";
 import {
   CommentList,
   Container,
@@ -19,91 +14,22 @@ import {
   UserAvatarOptionButton,
   UserAvatarOptionLink
 } from "./styles";
-import CommentInput from "@/components/molecules/CommentInput";
-import LoadingPage from "../LoadingPage";
+import { useCommentArea } from "./useCommentArea";
 
 const CommentArea = () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const url = urlParams.get("url");
-  const projectSecretKey = urlParams.get("projectKey");
-
-  const [sortOption, setSortOption] = useState<keyof typeof ORDER_BUTTON>("oldest");
-  const [notice, setNotice] = useState("");
-
-  const { refetchAccessToken } = useToken();
-  const { user, logout } = useUser();
-  const {
-    totalCommentsCount,
-    comments,
-    refetch: refetchAllComments,
-    isLoading: commentsLoading,
-    error: commentsError
-  } = useGetAllComments({ url, projectSecretKey, sortOption });
   const {
     projectOwnerId,
-    isLoading: getProjectOwnerIdLoading,
-    error: getProjectOwnerError
-  } = useGetProjectOwnerId(projectSecretKey || "");
-  const { setScrollHeight } = useMessageChannelFromReplyModuleContext();
-
-  useEffect(() => {
-    setScrollHeight();
-  }, [comments]);
-
-  useEffect(() => {
-    refetchAllComments();
-  }, [sortOption]);
-
-  useEffect(() => {
-    if (getProjectOwnerIdLoading || commentsLoading) return;
-
-    if (!url) {
-      setNotice("URL을 확인해주세요.");
-      return;
-    }
-    if (!projectSecretKey) {
-      setNotice("project secret key를 확인해주세요.");
-      return;
-    }
-    if (getProjectOwnerError) {
-      setNotice(getProjectOwnerError.message);
-      return;
-    }
-    if (commentsError) {
-      setNotice(commentsError.message);
-      return;
-    }
-
-    if (!(getProjectOwnerError || commentsError)) {
-      if (totalCommentsCount === 0) {
-        setNotice("작성된 댓글이 없습니다.");
-        return;
-      }
-
-      setNotice("");
-    }
-  }, [getProjectOwnerIdLoading, commentsLoading, getProjectOwnerError, commentsError, totalCommentsCount]);
-
-  const onLogin = async (provider: keyof typeof OAUTH_URL) => {
-    try {
-      const popUp = popUpCenter(OAUTH_URL[provider], "Authentication", 600, 900, "modal=yes,alwaysRaised=yes");
-
-      const timerId = setInterval(() => {
-        if (!popUp || !popUp.closed) return;
-
-        clearInterval(timerId);
-        refetchAccessToken();
-      }, 300);
-    } catch (error) {
-      if (error instanceof AlertError) {
-        alert(error.message);
-      }
-    }
-  };
-
-  const onSelectSortOption = (sortOption: keyof typeof ORDER_BUTTON) => {
-    setSortOption(sortOption);
-  };
+    getProjectOwnerIdLoading,
+    commentsLoading,
+    user,
+    totalCommentsCount,
+    comments,
+    sortOption,
+    onSelectSortOption,
+    notice,
+    logout,
+    onLogin
+  } = useCommentArea();
 
   return (
     <Container>
