3bd284c2bc0bbb3d966ddf727e2465b049924cdc
zereight(doby) <fullstackmachinelearning@gmail.com>
2021-10-09 01:18:58 +0900

    fix: 시크릿모드에서 localStorage 접근 불가시 예외 처리

--- frontend/manage/src/utils/localStorage.ts
+++ frontend/manage/src/utils/localStorage.ts
@@ -12,21 +12,33 @@ const getCircularReplacer = () => {
 };
 
 export const getLocalStorage = (key: string) => {
-  const value = localStorage.getItem(key);
+  try {
+    const value = localStorage.getItem(key);
 
-  if (typeof value === "string") return JSON.parse(value);
+    if (typeof value === "string") return JSON.parse(value);
+  } catch (error) {
+    return undefined;
+  }
 };
 
 export const setLocalStorage = (key: string, value: any) => {
-  const decycled = JSON.stringify(value, getCircularReplacer());
+  try {
+    const decycled = JSON.stringify(value, getCircularReplacer());
 
-  if (decycled) {
-    localStorage.setItem(key, decycled);
-  } else {
-    console.error("JSON stringify error.");
+    if (decycled) {
+      localStorage.setItem(key, decycled);
+    } else {
+      console.error("JSON stringify error.");
+    }
+  } catch (error) {
+    return null;
   }
 };
 
 export const removeLocalStorage = (key: string) => {
-  localStorage.removeItem(key);
+  try {
+    localStorage.removeItem(key);
+  } catch (error) {
+    return null;
+  }
 };
