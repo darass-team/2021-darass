ed8a4670841c23c2e48b77be49497cdaf4904dd9
JinYoung Park <basejin0810@naver.com>
2021-10-09 12:18:33 +0900

    feat: CommentTest 작성

--- backend/src/test/java/com/darass/comment/domain/CommentTest.java
+++ backend/src/test/java/com/darass/comment/domain/CommentTest.java
@@ -7,6 +7,9 @@ import com.darass.exception.ExceptionWithMessageAndCode;
 import com.darass.project.domain.Project;
 import com.darass.user.domain.GuestUser;
 import com.darass.user.domain.SocialLoginUser;
+import com.darass.user.domain.User;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,7 +23,9 @@ class CommentTest {
     private GuestUser guestUser;
     private SocialLoginUser socialLoginUser;
     private Project project;
-    private Comment comment;
+    private Comment commentWithGuestUser;
+    private Comment commentWithLoginUser;
+    private List<Comment> subComments = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
@@ -41,45 +46,53 @@ class CommentTest {
             .name("깃헙 지킬 블로그")
             .build();
 
-        comment = createComment(content);
+        commentWithGuestUser = createComment(content, guestUser, null,false);
+        commentWithLoginUser = createComment(content, socialLoginUser, null, true);
+
+        subComments.add(createComment(content, guestUser, commentWithLoginUser, false));
+        subComments.add(createComment(content, socialLoginUser, commentWithLoginUser, false));
+        subComments.add(createComment(content, guestUser, commentWithLoginUser, true));
+        subComments.add(createComment(content, socialLoginUser, commentWithLoginUser, true));
+
+        commentWithLoginUser.getSubComments().addAll(subComments);
     }
 
     @DisplayName("changeContent 메서드는 content가 주어지면, content를 바꾸고 아무것도 반환하지 않는다.")
     @Test
     void changeContent() {
-        comment.changeContent("수정된 댓글 내용");
+        commentWithGuestUser.changeContent("수정된 댓글 내용");
 
-        assertThat(comment.getContent()).isNotEqualTo(content);
+        assertThat(commentWithGuestUser.getContent()).isNotEqualTo(content);
     }
 
     @DisplayName("isCommentWriter 메서드는 댓글을 작성한 유저가 주어지면, true를 반환한다.")
     @Test
     void isCommentWriter_true() {
-        assertThat(comment.isCommentWriter(guestUser)).isTrue();
+        assertThat(commentWithGuestUser.isCommentWriter(guestUser)).isTrue();
     }
 
     @DisplayName("isCommentWriter 메서드는 댓글을 작성하지 않은 유저가 주어지면, false를 반환한다.")
     @Test
     void isCommentWriter_false() {
-        assertThat(comment.isCommentWriter(new GuestUser())).isFalse();
+        assertThat(commentWithGuestUser.isCommentWriter(new GuestUser())).isFalse();
     }
 
     @DisplayName("match 메서드는 댓글에 해당하는 url과 projectKey가 주어지면, true를 반환한다.")
     @Test
     void match_true() {
-        assertThat(comment.match(url, project.getSecretKey())).isTrue();
+        assertThat(commentWithGuestUser.match(url, project.getSecretKey())).isTrue();
     }
 
     @DisplayName("match 메서드는 댓글에 해당하지 않는 url이 주어지면, false를 반환한다.")
     @Test
     void match_url_false() {
-        assertThat(comment.match("invalid url", project.getSecretKey())).isFalse();
+        assertThat(commentWithGuestUser.match("invalid url", project.getSecretKey())).isFalse();
     }
 
     @DisplayName("match 메서드는 댓글에 해당하지 않는 projectKey가 주어지면, false를 반환한다.")
     @Test
     void match_projectKey_false() {
-        assertThat(comment.match(url, "invalid projectKey")).isFalse();
+        assertThat(commentWithGuestUser.match(url, "invalid projectKey")).isFalse();
     }
 
     @DisplayName("addCommentLike 메소드는 좋아요를 추가한다.")
@@ -87,18 +100,18 @@ class CommentTest {
     void addCommentLike() {
         CommentLike commentLike = buildCommentLike();
 
-        comment.addCommentLike(commentLike);
-        assertThat(comment.getCommentLikes()).hasSize(1);
+        commentWithGuestUser.addCommentLike(commentLike);
+        assertThat(commentWithGuestUser.getCommentLikes()).hasSize(1);
     }
 
     @DisplayName("deleteCommentLikeByUser 메소드는 좋아요를 제거한다.")
     @Test
     void deleteCommentLike() {
         CommentLike commentLike = buildCommentLike();
-        comment.addCommentLike(commentLike);
+        commentWithGuestUser.addCommentLike(commentLike);
 
-        comment.deleteCommentLikeByUser(socialLoginUser);
-        assertThat(comment.getCommentLikes()).hasSize(0);
+        commentWithGuestUser.deleteCommentLikeByUser(socialLoginUser);
+        assertThat(commentWithGuestUser.getCommentLikes()).hasSize(0);
     }
 
     @DisplayName("제한길이를 초과하는 댓글을 생성하는 경우 예외가 발생한다.")
@@ -108,24 +121,49 @@ class CommentTest {
         IntStream.rangeClosed(0, 1000)
             .forEach(it -> stringBuilder.append("str"));
 
-        assertThatThrownBy(() -> createComment(stringBuilder.toString()))
+        assertThatThrownBy(() -> createComment(stringBuilder.toString(), guestUser, null, false))
             .isInstanceOf(ExceptionWithMessageAndCode.INVALID_INPUT_LENGTH.getException().getClass());
+    }
+
+    @DisplayName("유저가 로그인 상태라면, 비밀 댓글의 유저 닉네임과 본문을 숨긴다.")
+    @Test
+    void replaceCommentInfoToSecret_login_user() {
+        commentWithLoginUser.replaceCommentInfoToSecret();
+        assertThat(commentWithLoginUser.getUserNickName()).isEqualTo(Comment.SECRET_COMMENT_USER_NICKNAME);
+        assertThat(commentWithLoginUser.getContent()).isEqualTo(Comment.SECRET_COMMENT_CONTENT);
+    }
+
+    @DisplayName("유저가 비로그인 상태라면, 비밀 댓글의 본문만 숨긴다.")
+    @Test
+    void replaceCommentInfoToSecret_guest_user() {
+        commentWithGuestUser.replaceCommentInfoToSecret();
+        assertThat(commentWithGuestUser.getUserNickName()).isNotEqualTo(Comment.SECRET_COMMENT_USER_NICKNAME);
+        assertThat(commentWithGuestUser.getContent()).isEqualTo(Comment.SECRET_COMMENT_CONTENT);
+    }
+
+    @DisplayName("각 부모 댓글과 대댓글에 대해 비밀 댓글이면 숨김 처리를 한다.")
+    @Test
+    void handleSecretComment() {
+        commentWithLoginUser.handleSecretComment();
+        assertThat(commentWithLoginUser.getSubComments()).hasSize(4);
     }
 
     private CommentLike buildCommentLike() {
         return CommentLike.builder()
-            .comment(comment)
+            .comment(commentWithGuestUser)
             .user(socialLoginUser)
             .build();
     }
 
-    private Comment createComment(String content) {
+    private Comment createComment(String content, User user, Comment parentComment, boolean secret) {
         return Comment.builder()
             .id(1L)
-            .user(guestUser)
+            .user(user)
             .project(project)
             .url(url)
             .content(content)
+            .parent(parentComment)
+            .secret(secret)
             .build();
     }
 }
