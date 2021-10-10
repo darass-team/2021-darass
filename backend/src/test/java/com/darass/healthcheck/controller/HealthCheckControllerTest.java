8370d42bbd38df33264dfb2a4a01adf61722140e
jaeseongDev <qkrwotjd1445@naver.com>
2021-10-11 02:54:56 +0900

    [BE] Helath Check API 추가 (#702)

---
+++ backend/src/test/java/com/darass/healthcheck/controller/HealthCheckControllerTest.java
@@ -0,0 +1,19 @@
+package com.darass.healthcheck.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.darass.darass.AcceptanceTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("HealthCheckController 클래스")
+public class HealthCheckControllerTest extends AcceptanceTest {
+
+    @DisplayName("/health GET으로 요청했을 때, 200으로 응답")
+    @Test
+    public void healthCheckTest() throws Exception {
+        mockMvc.perform(get("/health"))
+            .andExpect(status().isOk());
+    }
+}
