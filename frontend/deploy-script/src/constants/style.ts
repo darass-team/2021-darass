e8254d17b3d4e5bf2e75d9b3ca39ce0dabbe539a
zereight(doby) <fullstackmachinelearning@gmail.com>
2021-10-10 15:51:52 +0900

    fix: 댓글 모달에 z-index와 !important 속성 추가

--- frontend/deploy-script/src/constants/style.ts
+++ frontend/deploy-script/src/constants/style.ts
@@ -1,4 +1,4 @@
 export const IFRAME_STYLE = {
   REPLY_MODULE: `width: 100% !important; border: none !important; overflow: hidden !important; height: 540px !important;`,
-  MODAL: `position: fixed !important; top: 0; left: 0; width: 100vw; height: 100vh; transform: translateY(-100%); z-index=${Number.MAX_SAFE_INTEGER} !important;`
+  MODAL: `position: fixed !important; top: 0; left: 0; width: 100vw; height: 100vh; transform: translateY(-100%);`
 } as const;
