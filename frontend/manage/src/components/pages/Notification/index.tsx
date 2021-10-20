e5baa65b5995a9537869c5092d43b0edf5f35902
zereight(doby) <fullstackmachinelearning@gmail.com>
2021-10-20 23:41:48 +0900

    fix: 다크모드일때 댓글 이름/비밀번호 입력 색상 수정

--- frontend/manage/src/components/pages/Notification/index.tsx
+++ frontend/manage/src/components/pages/Notification/index.tsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 import { AlarmContainer, Container, Title } from "./styles";
 
 const Notification = () => {
-  const { data: alarmContents, setHasNewAlarmOnRealTime, isFetched: isFetchedGetAlarmContents } = useGetAlarmContents();
+  const { data: alarmContents, setHasNewAlarmOnRealTime } = useGetAlarmContents();
   const { editUser } = useEditUser();
   useDocumentTitle("알림");
 
@@ -26,7 +26,7 @@ const Notification = () => {
   };
 
   useEffect(() => {
-    if (isFetchedGetAlarmContents) {
+    if (alarmContents) {
       readAlarm();
     }
   }, [alarmContents]);
@@ -35,9 +35,7 @@ const Notification = () => {
     <ScreenContainer>
       <Container>
         <Title>알림</Title>
-        <AlarmContainer>
-          {isFetchedGetAlarmContents && <AlarmContent alarmContents={alarmContents || []} />}
-        </AlarmContainer>
+        <AlarmContainer>{alarmContents && <AlarmContent alarmContents={alarmContents || []} />}</AlarmContainer>
       </Container>
     </ScreenContainer>
   );
