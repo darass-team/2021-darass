d7122cec0edd9ea1637ad0929054b1c0068d98b0
zereight(doby) <fullstackmachinelearning@gmail.com>
2021-10-22 16:29:58 +0900

    refactor: PALLET enum제거

--- frontend/reply-module/src/constants/styles/palette.ts
+++ frontend/reply-module/src/constants/styles/palette.ts
@@ -1,28 +1,32 @@
-export enum PALETTE {
-  "PRIMARY" = "#10DF99",
-  "SECONDARY" = "#0BC586",
-  "TERTIARY" = "#FFE200",
-  "WHITE" = "#FFFFFF",
-  "BLACK_700" = "#303030",
-  "BLACK_900" = "#000000",
-  "GRAY_100" = "#f1f3f5",
-  "GRAY_200" = "#e9ecef",
-  "GRAY_300" = "#dee2e6",
-  "GRAY_400" = "#ced4da",
-  "GRAY_500" = "#B5B5B5",
-  "GRAY_600" = "#868e96",
-  "GRAY_700" = "#495057",
-  "GRAY_800" = "#343a40",
-  "RED_500" = "#EC2626",
-  "RED_600" = "#E41E1E",
-  "RED_800" = "#CB2431",
-  "INDIGO_600" = "#4c6ef5",
-  "BLUE_500" = "#005EFF",
-  "BLUE_700" = "#065FD4",
-  "TEAL_700" = "#0ca678",
-  "BAR" = GRAY_500,
-  "SECONDARY_HOVER" = TEAL_700,
-  "WHITE_HOVER" = GRAY_400,
-  "GRAY_HOVER" = GRAY_500,
-  "DEFAULT_BG" = GRAY_300
-}
+export const PALETTE = {
+  PRIMARY: "#0BC586",
+  WHITE: "#FFFFFF",
+  BLACK_700: "#303030",
+  BLACK_900: "#000000",
+  GRAY_100: "#f1f3f5",
+  GRAY_200: "#e9ecef",
+  GRAY_300: "#dee2e6",
+  GRAY_400: "#ced4da",
+  GRAY_500: "#B5B5B5",
+  GRAY_600: "#868e96",
+  GRAY_700: "#495057",
+  GRAY_800: "#343a40",
+  RED_500: "#EC2626",
+  RED_600: "#E41E1E",
+  RED_800: "#CB2431",
+  INDIGO_600: "#4c6ef5",
+  BLUE_500: "#005EFF",
+  BLUE_700: "#065FD4",
+  get BAR() {
+    return this.GRAY_500;
+  },
+  get WHITE_HOVER() {
+    return this.GRAY_400;
+  },
+  get GRAY_HOVER() {
+    return this.GRAY_500;
+  },
+  get DEFAULT_BG() {
+    return this.GRAY_300;
+  }
+} as const;
