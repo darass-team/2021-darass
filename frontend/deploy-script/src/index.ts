e2a6c61154d736af490d92e2741f286200481d55
zereight(doby) <fullstackmachinelearning@gmail.com>
2021-10-26 12:09:52 +0900

    fix: 기존사용자 대응

--- frontend/deploy-script/src/index.ts
+++ frontend/deploy-script/src/index.ts
@@ -24,15 +24,18 @@ const init = () => {
     return;
   }
 
-  const isDarkModePage = $darass.dataset.darkMode === "false" ? false : true;
+  const isDarkModePage =
+    $darass.dataset.darkMode === undefined ? false : $darass.dataset.darkMode === "true" ? true : false;
 
   const primaryColor = $darass.dataset.primaryColor || "#0BC586";
 
-  const isShowSortOption = $darass.dataset.showSortOption === "false" ? false : true;
+  const isShowSortOption =
+    $darass.dataset.showSortOption === undefined ? true : $darass.dataset.showSortOption === "true" ? true : false;
 
-  const isAllowSocialLogin = $darass.dataset.allowSocialLogin === "false" ? false : true;
+  const isAllowSocialLogin =
+    $darass.dataset.allowSocialLogin === undefined ? true : $darass.dataset.allowSocialLogin === "true" ? true : false;
 
-  const isShowLogo = $darass.dataset.showLogo === "false" ? false : true;
+  const isShowLogo = $darass.dataset.showLogo === undefined ? true : $darass.dataset.showLogo === "true" ? true : false;
 
   const $replyModuleIframe = createIframe(
     getReplyModuleURL({ projectKey, isDarkModePage, primaryColor, isShowSortOption, isAllowSocialLogin, isShowLogo }),
