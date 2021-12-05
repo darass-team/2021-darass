55abf052b4d5643b408070c84ab300e35ceacd83
JinYoung Park <basejin0810@naver.com>
2021-12-05 22:33:40 +0900

    feat: 마이그레이션용 설정 파일 추가

--- backend/src/main/java/com/darass/config/WebSocketConfig.java
+++ backend/src/main/java/com/darass/config/WebSocketConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Profile({"prod", "develop", "local"})
+@Profile({"prod", "develop", "local", "migration"})
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
